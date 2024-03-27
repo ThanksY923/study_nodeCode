428f4e0cabac08677e803b71dbe7898c5110639f
yao.chaofeng@detvista.com <1254821156@qq.com>
2024-03-27 11:12:28 +0800

    init

--- nodeFs模块基本使用/01_基本使用.js
+++ nodeFs模块基本使用/01_基本使用.js
@@ -2,35 +2,37 @@ const fs = require("fs");
 const path = require("path");
 
 // fs读取文件
-// 同步方式
-const res1 = fs.readFileSync("./aaa.txt", { encoding: "utf-8" });
-console.log(res1);
-
-// 回调函数异步方式
-const res2 = fs.readFile("./aaa.txt", { encoding: "utf-8" }, (err, res) => {
-	if (err) {
-		console.log(error);
-	} else {
-		console.log(res);
-	}
-});
-console.log("aaaaaa");
-
-// promise异步回调方式
-const res3 = fs.promises
-	.readFile("./aaa.txt", { encoding: "utf-8" })
-	.then(res => {
-		console.log(res);
-	})
-	.catch(err => {
-		console.log(err);
-	});
-console.log("bbbbbb");
-
-async function aaa() {
-	const res1 = await fs.promises.readFile("./aaa.txt", { encoding: "utf-8" });
-	console.log("cccccc");
+function getFile() {
+	// 同步方式
+	const res1 = fs.readFileSync("./aaa.txt", { encoding: "utf-8" });
 	console.log(res1);
-}
 
-aaa();
+	// 回调函数异步方式
+	const res2 = fs.readFile("./aaa.txt", { encoding: "utf-8" }, (err, res) => {
+		if (err) {
+			console.log(error);
+		} else {
+			console.log(res);
+		}
+	});
+	console.log("aaaaaa");
+
+	// promise异步回调方式
+	const res3 = fs.promises
+		.readFile("./aaa.txt", { encoding: "utf-8" })
+		.then(res => {
+			console.log(res);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	console.log("bbbbbb");
+
+	async function aaa() {
+		const res1 = await fs.promises.readFile("./aaa.txt", { encoding: "utf-8" });
+		console.log("cccccc");
+		console.log(res1);
+	}
+
+	aaa();
+}
